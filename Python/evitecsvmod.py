d2b4a9dcb8a64757b8ebad69e46f71a0ed164148
jonhayjr <33227538+jonhayjr@users.noreply.github.com>
2019-06-20 22:24:45 -0700

    Add files via upload

---
+++ Python/evitecsvmod.py
@@ -0,0 +1,21 @@
+--Transfers contens of guestlist.csv to new file eviteguests.csv
+--Removes files with filename of guestlist.csv and RSVPUpdates.csv
+import os
+import csv
+oldcsv = open('C:\\Users\\Haymaker17\\Downloads\\guestlist.csv', 'r', encoding = 'utf-8')
+oldreader = csv.reader(oldcsv, delimiter = ',')
+newcsv = open('C:\\Import\\eviteguests.csv', 'w', encoding = 'utf-8', newline = '')
+newwriter = csv.writer(newcsv)
+for row in oldreader:
+
+	newwriter.writerow ([row[0], row[1], row[2], row[3], row[4], row[5]])
+
+
+oldcsv.close()
+newcsv.close()
+
+os.remove('C:\\Users\\Haymaker17\\Downloads\\guestlist.csv')
+os.remove('C:\\Exports\\RSVPUpdates.csv')
+print("Files Removed!")
+
+
---
+++ Python/rsvpemail.py
@@ -0,0 +1,42 @@
+--Emails RSVPUpdates.csv which shows all updates that have been made.
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
+email_user = 'jonhayjr@gmail.com'
+email_password = 'Haymaker17'
+email_send = 'jonhayjr@gmail.com'
+
+subject = 'Modified Guest RSVPs and Notes'
+
+msg = MIMEMultipart()
+msg['From'] = email_user
+msg['To'] = email_send
+msg['Subject'] = subject
+
+body = 'Hi Jon!  Here are the modified RSVPs and notes for today.'
+msg.attach(MIMEText(body,'plain'))
+
+filename='C:\\Exports\\RSVPUpdates.csv'
+attachment  =open(filename,'rb')
+
+part = MIMEBase('application','octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition',"attachment; filename= "+filename)
+
+try:
+	msg.attach(part)
+	server = smtplib.SMTP_SSL('smtp.googlemail.com', 465)
+	server.login(email_user, email_password)
+	text = msg.as_string()
+	server.sendmail(email_user, email_send, text)
+	server.quit()
+
+	print ("Email sent!")
+
+except:
+
+	print ("Something went wrong.")
